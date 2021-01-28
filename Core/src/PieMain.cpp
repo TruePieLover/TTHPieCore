9fff648eeb7ad0e4c576e9b61a2f65e69fbc8e82
TruePieLover <brandonteich123@gmail.com>
2021-01-27 20:11:37 -0500

    first

---
+++ Core/src/PieMain.cpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////
+//      TTHPieCore
+// Project: TTHPieCore
+// Author: Pie
+// Creation date: 1/25/2021
+// Module: Source
+// File: PieMain.cpp
+// Description: Main source file
+///////////////////////////////////////
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome\n";
+    std::cin.get();
+    return 0;
+}
